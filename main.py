4ce2dd816b0268f1aa67764ee112a8e040afa95b
kushpinder <kushpindersingh15@gmail.com>
2026-06-06 12:01:29 +0530

    Complete A1 v0.1 terminal task manager

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
 from modules.task_manager import *
 
 
-delete_task(2)
-show_tasks()
+print("A1 Active")
+while True:
+    
+    
+    command = input("A1 >")
+
+    if command == "show":
+        show_tasks()
+
+    elif command == "add":
+        title=input("Enter title:")  
+        add_task(title)
+
+    elif command == "complete":
+        ID=int(input("Enter task ID:"))
+        complete_task(ID)  
+        
+    elif command == "delete":
+        ID=int(input("Enter task ID"))
+        delete_task(ID)
+    
+    elif command == "exit":
+        print("A1 Terminated")
+        break 
+
+    elif command == "help":
+        print("""commands:
+              show
+              add
+              delete
+              complete
+              help
+              exit""")
+    else:
+        print("unkown command. Try help :)")    
+                   
